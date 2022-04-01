524d41dbfc50c44505659010a4fea3025ccb19cb
Bogdan <bagration1998@gmail.com>
2022-04-02 00:23:36 +0300

    123

--- components/HeadContent/HeadContent.js
+++ components/HeadContent/HeadContent.js
@@ -2,8 +2,8 @@ const HeadContent = ({title, description, keywords}) => {
     return (
         <div>
             <link rel="icon" href="/favicon.ico" />
-            <meta charSet="utf-8"/>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+            {/*<meta charSet="utf-8"/>*/}
+            {/*<meta name="viewport" content="initial-scale=1.0, width=device-width"/>*/}
             <title>{title}</title>
             <meta name="description" content={description}/>
             <meta name="keywords" content={keywords}/>
