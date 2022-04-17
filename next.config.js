482be6241ab4d228f7299aa52abcdf8d53b09b55
Bogdan <bagration1998@gmail.com>
2022-04-18 02:15:22 +0300

    add cases images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: [`${process.env.IMG_HOST}`],
+    domains: ['localhost'],
   },
 }
 
--- pages/cases/index.jsx
+++ pages/cases/index.jsx
@@ -3,65 +3,59 @@ import MainLayout from "../../components/MainLayout";
 import styles from './Cases.module.scss';
 import Link from 'next/link';
 import Image from 'next/image'
-import axios from 'axios';
 
-const Cases = ({cases, error}) => {
+const Cases = ({cases}) => {
 
     const [filteredCases, setFilteredCases] = useState(cases);
-    // console.log(filteredCases);
-
-    if (error) {
-        return <div>An error occured: {error.message}</div>;
-    }
-    const cardStyleBg = {
-        // backgroundImage: "url(" + { Background } + ")"
-        backgroundImage: "url(" + "https://cdn.pixabay.com/photo/2021/09/02/16/48/cat-6593947_960_720.jpg" + ")"
-    };
 
     return (
         <MainLayout>
             <section className="container">
 
                 <div className={styles.cases}>
-                <h1>Наши кейсы</h1>
+                    <h1>Наши кейсы</h1>
 
                     <div className={styles.wrapper}>
 
                         {
-
-                            filteredCases?.map((cas)=>(
-                                <Link href={`/cases/${cas.attributes.title_unique}`}>
-                                    <div key={cas.id} className={styles.card} style={ cardStyleBg }>
+                            filteredCases?.map((cas) => (
+                                <Link key={cas.id} href={`/cases/${cas.attributes.title_unique}`}>
+                                    <div  className={styles.card}>
                                         <h2>{cas.attributes.title}</h2>
                                         <div className={styles.tags}>
-                                            <div className={styles.tag}>Дизайн</div>
-                                            <div className={styles.tag}>Разработка сайта</div>
+                                            {
+                                                cas.attributes.categories.data.map(tag => (
+                                                    <div className={styles.tag}>{tag.attributes.category}</div>
+                                                ))
+                                            }
                                         </div>
 
-                                        <div className={styles.overlay} />
-
+                                        <div className={styles.overlay}/>
+                                        <Image
+                                            className={styles.img}
+                                            width="440"
+                                            height="308"
+                                            objectFit={'cover'}
+                                            layout='fill'
+                                            src={`http://localhost:1337${cas.attributes.preview_img.data.attributes.url}`}
+                                        />
                                     </div>
+
                                 </Link>
                             ))
                         }
                     </div>
-
-
                 </div>
             </section>
         </MainLayout>
     );
 };
 
-Cases.getInitialProps = async ctx => {
-    try {
-        const res = await axios.get('http://localhost:1337/api/cases');
-        const cases = res.data;
-        return { cases: cases.data };
-    } catch (error) {
-        return { error };
+Cases.getInitialProps = async (ctx) => {
+    const res = await fetch(`http://localhost:1337/api/cases?populate=*`)
+    const json = await res.json()
+    return {
+        cases: json.data
     }
-};
-
-
+}
 export default Cases;
