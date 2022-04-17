4d2864b6d17e5d8f10942a84829307c815827401
Bogdan <bagration1998@gmail.com>
2022-04-17 14:15:00 +0300

    add react-fast-marquee

--- components/MainPage/PartnersBlock/PartnersBlock.jsx
+++ components/MainPage/PartnersBlock/PartnersBlock.jsx
@@ -1,13 +1,35 @@
 import styles from './PartnersBlock.module.scss';
+import Marquee from "react-fast-marquee";
+import Image from 'next/image'
+import { partners } from "./partners";
 
 const PartnersBlock = () => {
+
     return (
         <div>
             <div className={styles.partnersBlock}>
-                <div className={'container'}>
-                    <span>partnersBlock</span>
-
-                </div>
+                {/*<div className={'container'}>*/}
+                <Marquee
+                    // pauseOnHover="true"
+                    speed={30}
+                    gradient="true"
+                    gradientColor={[235, 238, 245]}
+                    gradientWidth={200}
+                    loop={0}
+                >
+                    <div className={styles.partners}>
+                        {
+                            partners.map((partner) => (
+                                <Image
+                                    src={partner.path}
+                                    alt={partner.desc}
+                                    objectFit={'contain'}
+                                />
+                            ))
+                        }
+                    </div>
+                </Marquee>
+                {/*</div>*/}
             </div>
         </div>
     );
---
+++ components/MainPage/PartnersBlock/partners.js
@@ -0,0 +1,39 @@
+import pImplant from '../../../public/partners/implant.png'
+import pXrf from '../../../public/partners/xrf.png'
+import pAvangard from '../../../public/partners/avan.png'
+import pHyper from '../../../public/partners/hyper.png'
+import pLabelme from '../../../public/partners/lm_1.png'
+import pProfit from '../../../public/partners/lm_2.png'
+import pMangup from '../../../public/partners/mangup.png'
+
+
+export const partners = [
+    {
+        path: pImplant,
+        desc: 'implant'
+    },
+    {
+        path: pXrf,
+        desc: 'XRay Food'
+    },
+    {
+        path: pProfit,
+        desc: 'LabelMe Profit'
+    },
+    {
+        path: pAvangard,
+        desc: 'AvangardHR'
+    },
+    {
+        path: pHyper,
+        desc: 'Hyper'
+    },
+    {
+        path: pLabelme,
+        desc: 'LabelMe'
+    },
+    {
+        path: pMangup,
+        desc: 'Mangup Estate'
+    },
+]
